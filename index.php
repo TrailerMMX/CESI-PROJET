a7599c263a8f2b3471d4f2adab9b1808b284940e
antoine <zizi@orange.fr>
2018-03-12 12:01:26 +0100

    fd

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Toshiba
+ * Date: 12/03/2018
+ * Time: 11:35
+ */
+?>
+<?php
+//Ceci est un commentaire
+$toto = "Ceci est une chaine";
+echo $toto ;
+
